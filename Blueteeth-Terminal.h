0a8bad8e7c27489ebb6f6c7d260448c73059aa84
Ali-Doge <90266104+Ali-Doge@users.noreply.github.com>
2023-12-08 21:22:57 -0500

    BluetoothSerial Attempt unsuccesful

--- Blueteeth-Terminal.h
+++
@@ -1,7 +0,0 @@
-#include "BLEDevice.h"
-#include "BLEServer.h"
-#include "BLEUtils.h"
-#include "BLE2902.h"
-#include "ctype.h"
-
-#define MAX_INPUT_BUFFER 100
--- Blueteeth-Terminal.ino
+++ Blueteeth-Terminal.ino
@@ -1,225 +1,39 @@
-#include "Blueteeth-Terminal.h"
+// This example code is in the Public Domain (or CC0 licensed, at your option.)
+// By Evandro Copercini - 2018
+//
+// This example creates a bridge between Serial and Classical Bluetooth (SPP)
+// and also demonstrate that SerialBT have the same functionalities of a normal Serial
+// Note: Pairing is authenticated automatically by this device
 
-using namespace std;
+#include "BluetoothSerial.h"
 
-// BLE Terminal constants
-BLEServer *pServer = NULL;
-BLECharacteristic * pTxCharacteristic;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint8_t txValue = 0;
+String device_name = "Blueteeth-Terminal";
 
-#define SERVICE_UUID        "33bf5a58-a402-425c-9133-75bbb8ec7de5" // UART service UUID
-#define CHARACTERISTIC_UUID "33bf5a58-a402-425c-9133-75bbb8ec7de5"
+// Check if Bluetooth is available
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+  #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
-/*
-* Scan for BLE servers and find the first one that advertises the service we are looking for.
-*/
+// Check Serial Port Profile
+#if !defined(CONFIG_BT_SPP_ENABLED)
+  #error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
+#endif
 
-void scan(){
-  BLEScan* pBLEScan = BLEDevice::getScan(); //create new scan
-  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
-  BLEScanResults foundDevices = pBLEScan->start(5); //5 second scan duration
-  int count = foundDevices.getCount();
-  Serial.print("Found ");
-  Serial.print(count);
-  Serial.println(" devices");
-  for (int i = 0; i < count; i++) {
-    BLEAdvertisedDevice d = foundDevices.getDevice(i);
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.print(d.getAddress().toString().c_str());
-    Serial.print(" ");
-    Serial.println(d.getName().c_str());
-  }
-
-  // Reconnect to the last connected device
-  if (!deviceConnected) {
-    Serial.println("DEBUG: Restarting advertising...");
-    pServer->startAdvertising(); // restart advertising
-  }
-
-}
-
-string receiveFromMaster() {
-  delay(100);
-  int bufferPos = 0;
-  char inputBuffer[MAX_INPUT_BUFFER];
-  string masterResponse;
-  while (Serial.available() && bufferPos < MAX_INPUT_BUFFER) {
-    inputBuffer[bufferPos] = Serial.read();
-    if (inputBuffer[bufferPos] == '\r') {
-      inputBuffer[bufferPos] = '\0';
-      masterResponse = inputBuffer;
-      bufferPos = 0;
-      //clear the input buffer
-      inputBuffer[0] = '\0';
-      return masterResponse;
-    }
-    else {
-      Serial.printf("%c", inputBuffer[bufferPos]);
-      bufferPos++;
-    }
-  }
-  return masterResponse;
-}
-
-/*
-*  Handle the input that arrived from BLESerial
-*/
-void terminalInput(string input) {
-  string masterResponse;
-  if (input.compare("help") == 0) {
-      pTxCharacteristic->setValue("help command");
-      pTxCharacteristic->notify();
-    /*
-      Serial.println("BlueteethTerminal");
-      Serial.println("help - Show this help");
-
-      // Terminal/Scanner Functionality
-      Serial.println("ping - Ping the slaves to get the BlueteethSlaveAddresses");
-      Serial.println("scan - Scan for speakers");
-      Serial.println("select a,b,c - Select speakers from the scan");
-      Serial.println("list - Print the list of the selected speakers as an enumerated map");
-
-      // Master connection functionality
-      Serial.println("connect - Send the the list to BlueteethMaster and connect to the selected speakers");
-      Serial.println("disconnect - Disconnect a speaker from the list");
-      */
-  }
-  else if (input.compare("ping") == 0) {
-      pTxCharacteristic->setValue("ping command");
-      pTxCharacteristic->notify();
-      Serial.println("DEBUG: Pinging slaves..."); 
-      // UART ping request sent to Master
-      Serial.println("ping");
-
-      // Wait for response from Master
-      Serial.println("DEBUG: Enter some data: ");
-      masterResponse = receiveFromMaster();
-      
-      Serial.println("DEBUG: master: ");
-
-      Serial.println(masterResponse.c_str());
-
-
-      pTxCharacteristic->setValue(masterResponse.c_str());
-      pTxCharacteristic->notify();
-
-
-  }
-  else if (input.compare("scan") == 0) {
-      Serial.println("DEBUG: Scanning for speakers...");
-
-
-      pTxCharacteristic->setValue("scan command");
-      pTxCharacteristic->notify();
-      scan();
-  }
-  else if (input.compare("select") == 0) {
-      Serial.println("DEBUG: Selecting speakers...");
-      pTxCharacteristic->setValue("select command");
-      pTxCharacteristic->notify();
-      //select();
-  }
-  else if (input.compare("list") == 0) {
-      Serial.println("DEBUG: Listing speakers...");
-      pTxCharacteristic->setValue("list command");
-      pTxCharacteristic->notify();
-      //list();
-  }
-  else if (input.compare("connect") == 0) {
-      Serial.println("Connecting to speakers...");
-      pTxCharacteristic->setValue("connect command");
-      pTxCharacteristic->notify();
-
-      //connect(); //Send connect requests via UART 1-by-1 with the directive "connect BlueteethAddress,SpeakerName""
-  }
-  else if (input.compare("disconnect") == 0) {
-      Serial.println("Disconnecting from speakers...");
-      //disconnect(); //However this is done
-  }
-}
-
-
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-      Serial.println("DEBUG: Device connected!");  // Debugging data for the UART connection
-      pTxCharacteristic->setValue("TerminalConnected");
-      pTxCharacteristic->notify();
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      Serial.println("DEBUG: Device disconnected");  // Debugging data for the UART connection
-      deviceConnected = false;
-    }
-};
-
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      string rxValue = pCharacteristic->getValue();
-      for (int i = 0; i < rxValue.length(); i++) {
-        rxValue[i] = tolower(rxValue[i]); // Turn all characters lower case to avoid issues with case sensitivity
-      }
-      terminalInput(rxValue);
-    }
-};
-
+BluetoothSerial SerialBT;
 
 void setup() {
-
-  //Start Serial comms
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
-  Serial.println("Advertising as BlueteethTerminal to connect to a phone...");
-
-  BLEDevice::init("BlueteethTerminal");
-  
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  // Create a BLE Characteristic
-  pTxCharacteristic = pService->createCharacteristic(
-			CHARACTERISTIC_UUID,
-			BLECharacteristic::PROPERTY_READ    | 
-      BLECharacteristic::PROPERTY_WRITE   |
-      BLECharacteristic::PROPERTY_NOTIFY  |
-      BLECharacteristic::PROPERTY_INDICATE
-			);
-
-  BLEDescriptor *pTxNameDescriptor = new BLEDescriptor(BLEUUID((uint16_t)0x2901));
-  pTxNameDescriptor->setValue("BlueteethTerminal");
-
-  BLE2902* p2902Descriptor = new BLE2902();
-  p2902Descriptor->setNotifications(true); 
-  pTxCharacteristic->addDescriptor(p2902Descriptor);
-  pTxCharacteristic->addDescriptor(pTxNameDescriptor);
-  pTxCharacteristic->setCallbacks(new MyCallbacks());
-
-  // Start the service
-  pService->start();
-
-  // Start advertising
-  pServer->getAdvertising()->addServiceUUID(SERVICE_UUID);
-  pServer->getAdvertising()->start();
-  Serial.println("Waiting a client connection to notify...");
-
+  SerialBT.begin(device_name); //Bluetooth device name
+  //SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
+  Serial.printf("The device with name \"%s\" is started.\n\rNow you can pair it with Bluetooth!\n\r", device_name.c_str());
 }
 
 void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
 }
-
-
-
-
-
-
-
-
-
-
